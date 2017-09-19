3bee157d98afa6b0303ae8d88f3ca8db05d51371
StasZherebkin <s.zherebkin@keepsolid.com>
2017-09-19 11:51:05 +0300

    initial commit, begin adding gesture area

---
+++ Gestobot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gestobot
+//
+//  Created by user on 9/19/17.
+//  Copyright © 2017 StasZherebkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
